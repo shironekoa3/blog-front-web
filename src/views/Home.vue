71d2e8a39662f285e89c9c6aa1bf3c661eb15650
shironekoa3 <shironekoa3@qq.com>
2023-02-23 23:30:50 +0800

    init project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,17 +6,6 @@
 </template>
   
 <script>
-// import { reactive, toRefs, onMounted } from "vue";
-// import { useRouter, useRoute } from "vue-router";
-// import { listHomeBooks } from "@/api/home";
-// import { ElMessage, ElLoading } from "element-plus";
-// import Header from "@/components/common/Header";
-// import Footer from "@/components/common/Footer";
-// import LatestNews from "@/components/home/LatestNews";
-// import FriendLink from "@/components/home/FriendLink";
-// import BookVisitRank from "@/components/home/BookVisitRank";
-// import BookNewestRank from "@/components/home/BookNewestRank";
-// import BookUpdateRank from "@/components/home/BookUpdateRank";
 export default {
     name: "home",
     components: {
